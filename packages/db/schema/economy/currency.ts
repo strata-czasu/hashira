e599948ac32261f0cc7669fb94cb9832587c8cd5
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-03-02 19:44:49 +0000

    Update currency table and add new tables for guild and emoji usage

--- packages/db/schema/economy/currency.ts
+++ packages/db/schema/economy/currency.ts
@@ -3,7 +3,7 @@ import { serial, text } from "drizzle-orm/pg-core";
 import { pgTable } from "../../pgtable";
 import { wallet } from "./wallet";
 
-export const currency = pgTable("currencies", {
+export const currency = pgTable("currency", {
 	id: serial("id").primaryKey(),
 	name: text("name"),
 	symbol: text("symbol"),
--- packages/db/schema/index.ts
+++ packages/db/schema/index.ts
@@ -1,2 +1,5 @@
 export * from "./economy";
 export * from "./user";
+export * from "./guild";
+export * from "./emojiUsage";
+export * from "./userActivity";
