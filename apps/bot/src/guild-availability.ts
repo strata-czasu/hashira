ed14165ccf80f22692c8a40d280632db73549885
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-03-02 19:49:24 +0000

    Add guild availability functionality that adds guild records to database

---
+++ apps/bot/src/guild-availability.ts
@@ -0,0 +1,18 @@
+import { Hashira } from "@hashira/core";
+import { guild } from "@hashira/db/schema";
+import { type PgInsertValue } from "drizzle-orm/pg-core";
+import { base } from "./base";
+
+export const guildAvailability = new Hashira({ name: "guild-availability" })
+	.use(base)
+	.handle("ready", async ({ db }, client) => {
+		const guilds = client.guilds.cache.map(
+			(discordGuild) =>
+				({ id: BigInt(discordGuild.id) }) as PgInsertValue<typeof guild>,
+		);
+
+		await db.insert(guild).values(guilds).onConflictDoNothing().returning();
+	})
+	.handle("guildCreate", async ({ db }, discordGuild) => {
+		await db.insert(guild).values({ id: BigInt(discordGuild.id) }).onConflictDoNothing().execute();
+	});
