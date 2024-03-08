6f88c732b917fb67e5e8d77540340d417ae8b3d7
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-03-08 02:25:39 +0000

    Added some nonNull constraints on database schemas

--- packages/db/src/schema/economy/currency.ts
+++ packages/db/src/schema/economy/currency.ts
@@ -5,8 +5,8 @@ import { wallet } from "./wallet";
 
 export const currency = pgTable("currency", {
 	id: serial("id").primaryKey(),
-	name: text("name"),
-	symbol: text("symbol"),
+	name: text("name").unique().notNull(),
+	symbol: text("symbol").unique().notNull(),
 });
 
 export const currencyRelations = relations(currency, ({ many }) => ({
