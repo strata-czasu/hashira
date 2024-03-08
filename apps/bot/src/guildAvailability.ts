3d63ba41f6c59a8664478cff9bbe44d846b939e5
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-03-08 02:00:15 +0000

    Fix previous usages of bigints

--- apps/bot/src/guildAvailability.ts
+++ apps/bot/src/guildAvailability.ts
@@ -1,14 +1,12 @@
 import { Hashira } from "@hashira/core";
-import { guild } from "@hashira/db";
-import { type PgInsertValue } from "drizzle-orm/pg-core";
+import { guild } from "@hashira/db/schema";
 import { base } from "./base";
 
 export const guildAvailability = new Hashira({ name: "guild-availability" })
 	.use(base)
 	.handle("ready", async ({ db }, client) => {
 		const guilds = client.guilds.cache.map(
-			(discordGuild) =>
-				({ id: BigInt(discordGuild.id) }) as PgInsertValue<typeof guild>,
+			(discordGuild) => ({ id: discordGuild.id }) as typeof guild.$inferInsert,
 		);
 
 		await db.insert(guild).values(guilds).onConflictDoNothing().returning();
@@ -16,7 +14,7 @@ export const guildAvailability = new Hashira({ name: "guild-availability" })
 	.handle("guildCreate", async ({ db }, discordGuild) => {
 		await db
 			.insert(guild)
-			.values({ id: BigInt(discordGuild.id) })
+			.values({ id: discordGuild.id })
 			.onConflictDoNothing()
 			.execute();
 	});
