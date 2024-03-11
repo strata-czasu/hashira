be8989e483c48ac3c59164b96884197ec6fe72bd
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-03-11 00:28:53 +0000

    Refactor Paginate class to accept configuration object

--- packages/db/src/paginate.ts
+++ packages/db/src/paginate.ts
@@ -1,33 +1,57 @@
 import { type SQL } from "drizzle-orm";
-import type { PgSelect, PgSelectBase } from "drizzle-orm/pg-core";
+import { PgSelectBase, type PgSelect } from "drizzle-orm/pg-core";
 import type { JoinNullability } from "drizzle-orm/query-builders/select.types";
 
-type CountSelect = PgSelectBase<
+type CountSelect = PgSelect<
 	string,
 	{ count: SQL<number> },
 	"partial",
-	Record<string, JoinNullability>,
-	false,
-	never,
-	{ count: number }[]
+	Record<string, JoinNullability>
 >;
+
+interface PaginateConfig<T extends PgSelect, U extends CountSelect> {
+	/**
+	 * Query for fetching the count of selected rows
+	 */
+	count?: U;
+	/**
+	 * Query for fetching the selected rows
+	 */
+	select: T;
+	/**
+	 * Page number to start from, zero-based
+	 * @default 0
+	 */
+	page?: number;
+	/**
+	 * Number of rows per page
+	 * @default 10
+	 */
+	pageSize?: number;
+}
 
 export class Paginate<T extends PgSelect, U extends CountSelect> {
 	private readonly qb: T;
-	private readonly countQb: U | undefined;
+	private readonly countQb: U | undefined = undefined;
 	private page = 0;
-	private readonly pageSize: number;
+	private readonly pageSize: number = 10;
 	private count: number = Number.MAX_SAFE_INTEGER;
-	constructor(qb: T, pageSize = 10, countQb: U | undefined = undefined) {
-		this.qb = qb;
-		this.countQb = countQb;
-		this.pageSize = pageSize;
+	constructor(config: T | PaginateConfig<T, U>) {
+		if (config instanceof PgSelectBase) {
+			this.qb = config;
+		} else {
+			this.qb = config.select;
+			this.countQb = config.count;
+			this.pageSize = config.pageSize ?? this.pageSize;
+			this.page = config.page ?? this.page;
+		}
 	}
 
 	private async fetchCount() {
-		if (this.count === null && this.countQb) {
+		if (this.count === Number.MAX_SAFE_INTEGER && this.countQb) {
 			const [row] = await this.countQb.execute();
 			if (row) this.count = row.count;
+			else this.count = 0;
 		}
 	}
 
