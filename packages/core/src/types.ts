f57a5e20edd540f3cdf48744f58e21f592bb7e11
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-03-17 02:15:19 +0000

    Refactor code structure and import statements in core

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,38 @@
+import type { AllEventsHandling } from "./intents";
+
 export type Prettify<T> = {
 	[K in keyof T]: T[K];
 } & {};
 
 export type MaybePromise<T> = T | Promise<T>;
+
+export type EventsWithContext<Context extends HashiraContext<HashiraDecorators>> = {
+	[K in keyof AllEventsHandling]: (
+		ctx: Context,
+		...args: Parameters<AllEventsHandling[K]>
+	) => MaybePromise<void>;
+};
+
+export type UnknownEventWithContext = (
+	ctx: unknown,
+	...args: unknown[]
+) => MaybePromise<void>;
+
+export type BaseDecorator = { [key: string]: unknown };
+
+export type HashiraContext<Decorators extends HashiraDecorators> = Prettify<
+	Decorators["const"] & Decorators["derive"] & { state: Decorators["state"] }
+>;
+
+export type UnknownContext = {
+	[key: string]: unknown;
+	state: { [key: string]: unknown };
+};
+
+export type UnknownDerive = (ctx: UnknownContext) => BaseDecorator;
+
+export type HashiraDecorators = {
+	const: BaseDecorator;
+	derive: BaseDecorator;
+	state: BaseDecorator;
+};
