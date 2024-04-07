a27e66a59d99e32b3f207067c159f3875c41d6a4
Daste <stefankar1000@gmail.com>
2024-04-07 18:26:47 +0200

    Add a autoRole db model

--- packages/db/src/schema/guild.ts
+++ packages/db/src/schema/guild.ts
@@ -1,6 +1,12 @@
 import { text } from "drizzle-orm/pg-core";
 import { pgTable } from "../pgtable";
+import { autoRole } from "./autorole";
+import { relations } from "drizzle-orm";
 
 export const guild = pgTable("guild", {
 	id: text("id").primaryKey(),
 });
+
+export const guildRelations = relations(guild, ({ many }) => ({
+	autoRoles: many(autoRole),
+}));
