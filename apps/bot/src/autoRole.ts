10c4dfd337e99d15560e210ae7da6a079fe0082c
Daste <stefankar1000@gmail.com>
2024-04-07 18:49:13 +0200

    Add auto role on user join functionality

---
+++ apps/bot/src/autoRole.ts
@@ -0,0 +1,96 @@
+import { Hashira, PaginatedView } from "@hashira/core";
+import { base } from "./base";
+import { eq, count } from "drizzle-orm";
+import { Paginate, schema } from "@hashira/db";
+
+export const autoRole = new Hashira({ name: "auto-role" })
+	.use(base)
+	.handle("guildMemberAdd", async ({ db }, member) => {
+		const autoRoles = await db
+			.select({ roleId: schema.autoRole.roleId })
+			.from(schema.autoRole)
+			.where(eq(schema.autoRole.guildId, member.guild.id));
+		await member.roles.add(
+			autoRoles.map(({ roleId }) => roleId),
+			"Auto role on join",
+		);
+	})
+	.group("autorole", (group) =>
+		group
+			.setDescription("Manage autoroles")
+			.addCommand("add", (command) =>
+				command
+					.setDescription("Add an autorole")
+					.addRole("role", (role) =>
+						role
+							.setDescription("Role that will be added to new users")
+							.setRequired(true),
+					)
+					.handle(async ({ db }, { role }, itx) => {
+						const added = await db
+							.insert(schema.autoRole)
+							.values({ guildId: role.guild.id, roleId: role.id })
+							.onConflictDoNothing()
+							.returning();
+						if (added.length === 0) {
+							await itx.reply({
+								content: `${role.name} is already in the autorole list`,
+								ephemeral: true,
+							});
+						} else {
+							await itx.reply({
+								content: `Added ${role.name} to autorole list`,
+								ephemeral: true,
+							});
+						}
+					}),
+			)
+			.addCommand("remove", (command) =>
+				command
+					.setDescription("Remove a role from the autorole list")
+					.addRole("role", (role) =>
+						role.setDescription("Role to remove from autorole list").setRequired(true),
+					)
+					.handle(async ({ db }, { role }, itx) => {
+						const removed = await db
+							.delete(schema.autoRole)
+							.where(eq(schema.autoRole.roleId, role.id))
+							.returning();
+						if (removed.length === 0) {
+							await itx.reply({
+								content: `${role.name} is not in the autorole list`,
+								ephemeral: true,
+							});
+						} else {
+							await itx.reply({
+								content: `Removed ${role.name} from autorole list`,
+								ephemeral: true,
+							});
+						}
+					}),
+			)
+			.addCommand("list", (command) =>
+				command.setDescription("List all autoroles").handle(async ({ db }, _, itx) => {
+					if (!itx.guildId) return;
+
+					const paginate = new Paginate({
+						select: db
+							.select({ roleId: schema.autoRole.roleId })
+							.from(schema.autoRole)
+							.where(eq(schema.autoRole.guildId, itx.guildId))
+							.$dynamic(),
+						count: db
+							.select({ count: count() })
+							.from(schema.autoRole)
+							.where(eq(schema.autoRole.guildId, itx.guildId))
+							.$dynamic(),
+					});
+					const paginatedView = new PaginatedView(
+						itx,
+						paginate,
+						(item, idx) => `${idx}. <@&${item.roleId}>`,
+					);
+					await paginatedView.render();
+				}),
+			),
+	);
--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -5,13 +5,15 @@ import { emojiCounting } from "./emojiCounting";
 import { guildAvailability } from "./guildAvailability";
 import { miscellaneous } from "./miscellaneous";
 import { userActivity } from "./userActivity";
+import { autoRole } from "./autoRole";
 
 export const bot = new Hashira({ name: "bot" })
 	.use(base)
 	.use(guildAvailability)
 	.use(emojiCounting)
 	.use(miscellaneous)
-	.use(userActivity);
+	.use(userActivity)
+	.use(autoRole);
 
 if (import.meta.main) {
 	// TODO: For docker, we need to handle SIGTERM, but because we use 'bun run' we don't
