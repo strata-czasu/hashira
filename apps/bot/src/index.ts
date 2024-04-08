f010ab25fcb0a2fa04ab9f591265360162f69254
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:40 +0000

    Add SIGTERM handler to stop the bot container in docker

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -3,14 +3,20 @@ import env from "@hashira/env";
 import { base } from "./base";
 import { emojiCounting } from "./emojiCounting";
 import { guildAvailability } from "./guildAvailability";
+import { paginationTests } from "./paginationTests";
 import { userActivity } from "./userActivity";
 
 export const bot = new Hashira({ name: "bot" })
 	.use(base)
 	.use(guildAvailability)
 	.use(emojiCounting)
-	.use(userActivity);
+	.use(userActivity)
+	.use(paginationTests);
 
 if (import.meta.main) {
+	// TODO: For docker, we need to handle SIGTERM
+	// This is dirty, but it works for now. We should gracefuly stop the bot
+	// along with its database.
+	process.on("SIGTERM", () => process.exit(1));
 	await bot.start(env.BOT_TOKEN);
 }
