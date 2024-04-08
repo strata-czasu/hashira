daad99c3a06178753618610163594c912c290611
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:39 +0000

    Added guildId, channelId, and timestamp to userActivity table

--- packages/db/src/schema/userActivity.ts
+++ packages/db/src/schema/userActivity.ts
@@ -1,18 +1,28 @@
 import { relations } from "drizzle-orm";
-import { serial, text } from "drizzle-orm/pg-core";
+import { serial, text, timestamp } from "drizzle-orm/pg-core";
 import { pgTable } from "../pgtable";
 import { user } from "./user";
+import { guild } from "./guild";
 
 export const userActivity = pgTable("user_activity", {
 	id: serial("id").primaryKey(),
 	userId: text("userId")
 		.references(() => user.id)
 		.notNull(),
+	guildId: text("guildId")
+		.references(() => guild.id)
+		.notNull(),
+	channelId: text("channelId").notNull(),
+	timestamp: timestamp("timestamp").defaultNow().notNull(),
 });
 
 export const userActivityRelations = relations(userActivity, ({ one }) => ({
 	user: one(user, {
 		fields: [userActivity.userId],
 		references: [user.id],
+	}),
+	guild: one(guild, {
+		fields: [userActivity.guildId],
+		references: [guild.id],
 	}),
 }));
