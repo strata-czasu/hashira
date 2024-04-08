051bd3dbbdffc30cb29b5e1e7aceb2d68e54ead0
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:39 +0000

    Add guild availability functionality that adds guild records to database

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,7 +1,9 @@
 import { Hashira } from "@hashira/core";
 import env from "@hashira/env";
+import { guildAvailability } from "./guild-availability";
 import { base } from "./base";
 
 await new Hashira({ name: "bot" })
 	.use(base)
+	.use(guildAvailability)
 	.start(env.BOT_TOKEN);
