3f7dc05cf01bb3e89c3b437df3747f6f7040c0e7
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:39 +0000

    Added fakerjs and created some basic testing utilities for database

--- packages/db/test/index.ts
+++ packages/db/test/index.ts
@@ -1,8 +1,11 @@
-import postgres from "postgres";
 import env from "@hashira/env";
-import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "../src";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle, type PostgresJsQueryResultHKT } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { schema } from "../src";
+import { faker } from "@faker-js/faker";
+import { test } from "bun:test";
+import { TransactionRollbackError, type ExtractTablesWithRelations } from "drizzle-orm";
+import type { PgTransaction } from "drizzle-orm/pg-core";
 
 const connection = postgres({
 	host: env.POSTGRES_TEST_HOST,
@@ -13,4 +16,38 @@ const connection = postgres({
 
 export const db = drizzle(connection, { schema });
 
-await migrate(db, { migrationsFolder: "drizzle" });
+export const fakerSnowflake = () =>
+	faker.string.numeric({ length: { min: 17, max: 20 }, allowLeadingZeros: false });
+
+export const createUser = (): typeof schema.user.$inferInsert => ({
+	id: fakerSnowflake(),
+});
+
+export const createGuild = (): typeof schema.guild.$inferInsert => ({
+	id: fakerSnowflake(),
+});
+
+export const dbTest = (
+	name: string,
+	exec: (
+		db: PgTransaction<
+			PostgresJsQueryResultHKT,
+			typeof schema,
+			ExtractTablesWithRelations<typeof schema>
+		>,
+	) => void | Promise<void>,
+) => {
+	test(name, async () => {
+		try {
+			await db.transaction(async (tx) => {
+				await exec(tx);
+				tx.rollback();
+			});
+		} catch (e) {
+			if (e instanceof TransactionRollbackError) {
+				return;
+			}
+			throw e;
+		}
+	});
+};
