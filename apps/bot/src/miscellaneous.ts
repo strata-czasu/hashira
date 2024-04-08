da12bc5753fe6a1be03189034ceb5814cefa92ae
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:40 +0000

    Add miscellaneous commands to the bot

---
+++ apps/bot/src/miscellaneous.ts
@@ -0,0 +1,76 @@
+import { Hashira } from "@hashira/core";
+import { AttachmentBuilder } from "discord.js";
+import { base } from "./base";
+
+const mentionRegex = /<@!?\d{15,20}>/;
+const idRegex = /\d{15,20}/;
+
+const getFromIdOrMention = (idOrMention: string) => {
+	const match = idOrMention.match(mentionRegex);
+	if (match) return match[1] ?? null;
+	const idMatch = idOrMention.match(idRegex);
+	if (idMatch) return idMatch[0] ?? null;
+	return null;
+};
+
+export const miscellaneous = new Hashira({ name: "miscellaneous" })
+	.use(base)
+	.group("misc", (group) =>
+		group
+			.setDefaultMemberPermissions(0)
+			.setDescription("Miscellaneous commands")
+			.addCommand("parse-statbot", (command) =>
+				command
+					.setDescription("Parse a Statbot output")
+					.addAttachment("csv", (option) =>
+						option.setDescription("The CSV file to parse"),
+					)
+					.handle(async (_, { csv }, itx) => {
+						if (csv.size > 100_000) return;
+						// rank,name,id,count
+						// 1,username_1,123456789012345678,100
+						const content = await fetch(csv.url).then((res) => res.text());
+						const lines = content.split("\n");
+						const ids = lines.slice(1).map((line) => {
+							const [_, __, id] = line.split(",");
+							return `<@${id}>`;
+						});
+
+						const attachment = new AttachmentBuilder(Buffer.from(ids.join(" ")), {
+							name: "parsed.txt",
+						});
+
+						await itx.reply({ files: [attachment] });
+					}),
+			)
+			.addCommand("add-role", (command) =>
+				command
+					.setDescription("Add a role to a list of users")
+					.addAttachment("users", (option) =>
+						option.setDescription("The users to add the role to"),
+					)
+					.addRole("role", (option) =>
+						option.setDescription("The role to add to the user"),
+					)
+					.handle(async (_, { users, role }, itx) => {
+						// Don't allow for more than 10 kilobytes of users
+						if (users.size > 20_000) return;
+						if (!itx.inCachedGuild()) return;
+						await itx.deferReply();
+						const content = await fetch(users.url).then((res) => res.text());
+						const ids = content
+							.split(/\s+/)
+							.map(getFromIdOrMention)
+							.filter((id): id is string => !!id);
+						await Promise.all(
+							ids.map(async (id) => {
+								const member = await itx.guild.members.fetch(id);
+								if (!member) return;
+								await member.roles.add(role);
+							}),
+						);
+
+						await itx.editReply("Added role to users");
+					}),
+			),
+	);
