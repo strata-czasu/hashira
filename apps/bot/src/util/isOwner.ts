ec12ba5a5609dbfbf459ee6530a35d8034163197
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:40 +0000

    Refactor isOwner function to use ChatInputCommandInteraction

--- apps/bot/src/util/isOwner.ts
+++ apps/bot/src/util/isOwner.ts
@@ -1,14 +1,20 @@
-import { Team, User } from "discord.js";
+import { ChatInputCommandInteraction, Team, User } from "discord.js";
 import { match } from "ts-pattern";
 
-export const isOwner = (userToMatch: User, entity: User | Team | null): boolean =>
-	match(entity)
+export const isOwner = async (
+	interaction: ChatInputCommandInteraction,
+): Promise<boolean> => {
+	const userToMatch = interaction.user;
+	const application = await interaction.client.application.fetch();
+
+	return match(application.owner)
 		.when(
-			(ent): ent is User => ent instanceof User,
+			(owner): owner is User => owner instanceof User,
 			(user) => user.id === userToMatch.id,
 		)
 		.when(
-			(ent): ent is Team => ent instanceof Team,
+			(owner): owner is Team => owner instanceof Team,
 			(team) => team.members.has(userToMatch.id),
 		)
 		.otherwise(() => false);
+};
