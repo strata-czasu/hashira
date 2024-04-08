b657df3740dbb175453ad5659e37cf92b8895b34
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:39 +0000

    Refactor Paginate class to accept configuration object

--- packages/db/test/paginate.test.ts
+++ packages/db/test/paginate.test.ts
@@ -3,15 +3,19 @@ import { faker } from "@faker-js/faker";
 import { schema } from "../src";
 import { Paginate } from "../src/paginate";
 import { createUser, dbTest } from "./";
+import { count } from "drizzle-orm";
 
 afterEach(async () => {
 	faker.seed();
 });
 
-dbTest("paginate", async (tx) => {
+dbTest("paginate unknown size", async (tx) => {
 	const users = faker.helpers.multiple(createUser, { count: 100 });
 	await tx.insert(schema.user).values(users);
-	const paginator = new Paginate(tx.select().from(schema.user).$dynamic(), 10);
+	const paginator = new Paginate({
+		select: tx.select().from(schema.user).$dynamic(),
+		pageSize: 10,
+	});
 	expect(await paginator.current()).toHaveLength(10);
 	expect(paginator.displayPages).toBe("?");
 	expect(paginator.displayCurrentPage).toBe("1");
@@ -42,6 +46,56 @@ dbTest("paginate", async (tx) => {
 	expect(paginator.displayCurrentPage).toBe("9");
 	expect(await paginator.next()).toHaveLength(10);
 	expect(paginator.displayPages).toBe("?");
+	expect(paginator.displayCurrentPage).toBe("10");
+
+	expect(await paginator.next()).toHaveLength(0);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("10");
+
+	expect(await paginator.next()).toHaveLength(0);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("10");
+});
+
+
+dbTest("paginate known size", async (tx) => {
+	const users = faker.helpers.multiple(createUser, { count: 100 });
+	await tx.insert(schema.user).values(users);
+	const paginator = new Paginate({
+		select: tx.select().from(schema.user).$dynamic(),
+		count: tx.select({ count: count() }).from(schema.user).$dynamic(),
+		pageSize: 10,
+	});
+	expect(await paginator.current()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("1");
+	expect(await paginator.prev()).toHaveLength(0);
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("2");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("3");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("4");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("5");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("6");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("7");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("8");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
+	expect(paginator.displayCurrentPage).toBe("9");
+	expect(await paginator.next()).toHaveLength(10);
+	expect(paginator.displayPages).toBe("10");
 	expect(paginator.displayCurrentPage).toBe("10");
 
 	expect(await paginator.next()).toHaveLength(0);
