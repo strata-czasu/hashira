5f452231c421f59300587e4e93754906e93beb44
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:40 +0000

    Added autocomplete endpoint to core and fixed improper merge of event handlers

---
+++ packages/core/src/utils.ts
@@ -0,0 +1,18 @@
+export const mergeMap = <K, V>(
+	onConflict: (a: V, b: V, key: K) => V,
+	...maps: Map<K, V>[]
+): Map<K, V> => {
+	const result = new Map<K, V>();
+	for (const map of maps) {
+		for (const [key, value] of map) {
+			const existing = result.get(key);
+			if (existing !== undefined) {
+				result.set(key, onConflict(existing, value, key));
+			} else {
+				result.set(key, value);
+			}
+		}
+	}
+	return result;
+};
+
