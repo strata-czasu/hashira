a809cfba259ded082883420d0510dc7a13c46230
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-08 19:52:39 +0000

    Added emoji counting module to bot

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -2,10 +2,12 @@ import { Hashira } from "@hashira/core";
 import env from "@hashira/env";
 import { guildAvailability } from "./guildAvailability";
 import { base } from "./base";
+import { emojiParsing } from "./emojiParsing";
 
 export const bot = new Hashira({ name: "bot" })
 	.use(base)
 	.use(guildAvailability)
+	.use(emojiParsing);
 
 if (import.meta.main) {
 	await bot.start(env.BOT_TOKEN);
