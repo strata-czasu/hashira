58cbbc01daef08b972e95ef971b714aa65ea0a17
Daste <stefankar1000@gmail.com>
2024-04-09 21:39:16 +0200

    Allow deleting User messages when banning them

--- apps/bot/src/moderation.ts
+++ apps/bot/src/moderation.ts
@@ -1,6 +1,11 @@
 import { Hashira } from "@hashira/core";
 import { formatDate } from "date-fns";
-import { DiscordAPIError, PermissionFlagsBits, User } from "discord.js";
+import {
+  DiscordAPIError,
+  PermissionFlagsBits,
+  RESTJSONErrorCodes,
+  User,
+} from "discord.js";
 
 const formatBanReason = (reason: string, moderator: User, createdAt: Date) =>
   `${reason} (banujący: ${moderator.tag} (${moderator.id}), \
@@ -8,22 +13,55 @@ data: ${formatDate(createdAt, "yyyy-MM-dd HH:mm:ss")})`;
 
 const formatUserWithId = (user: User) => `**${user.tag}** (\`${user.id}\`)`;
 
+enum BanDeleteInterval {
+  None = 0,
+  OneHour = 1,
+  SixHours = 6,
+  TwelveHours = 12,
+  OneDay = 24,
+  ThreeDays = 24 * 3,
+  SevenDays = 24 * 7,
+}
+
+const getBanDeleteSeconds = (deleteInterval: BanDeleteInterval) => {
+  return deleteInterval * 3600;
+};
+
 export const moderation = new Hashira({ name: "moderation" })
   .newCommand("ban", (command) =>
     command
       .setDescription("Zbanuj użytkownika")
       .addUser("user", (user) => user.setDescription("Użytkownik"))
       .addString("reason", (reason) => reason.setDescription("Powód bana"))
-      // TODO: Add dynamic delete time ranges
-      //       https://discord.js.org/docs/packages/discord.js/main/BanOptions:Interface#deleteMessageDays
-      .handle(async (_, { user, reason }, itx) => {
+      .addNumber("delete-interval", (deleteInterval) =>
+        deleteInterval
+          .setDescription("Przedział czasowy usuwania wiadomości")
+          .setRequired(false)
+          .addChoices(
+            { name: "Brak", value: BanDeleteInterval.None },
+            { name: "1 godzina", value: BanDeleteInterval.OneHour },
+            { name: "6 godzin", value: BanDeleteInterval.SixHours },
+            { name: "12 godzin", value: BanDeleteInterval.TwelveHours },
+            { name: "1 dzień", value: BanDeleteInterval.OneDay },
+            { name: "3 dni", value: BanDeleteInterval.ThreeDays },
+            { name: "7 dni", value: BanDeleteInterval.SevenDays },
+          ),
+      )
+      .handle(async (_, { user, reason, "delete-interval": deleteInterval }, itx) => {
         // TODO: Don't allow in DMs
-        if (!itx.guild || !itx.member) return;
+        if (!itx.inCachedGuild()) return;
         // TODO: Use default permissions on the slash command
-        if (!itx.memberPermissions?.has(PermissionFlagsBits.BanMembers)) return;
+        if (!itx.memberPermissions.has(PermissionFlagsBits.BanMembers)) return;
 
         const banReason = formatBanReason(reason, itx.user, itx.createdAt);
-        await itx.guild.members.ban(user, { reason: banReason });
+        if (!deleteInterval) {
+          await itx.guild.members.ban(user, { reason: banReason });
+        } else {
+          await itx.guild.members.ban(user, {
+            reason: banReason,
+            deleteMessageSeconds: getBanDeleteSeconds(deleteInterval),
+          });
+        }
 
         const message = `Zbanowano ${formatUserWithId(user)}. Powód: *${reason}*`;
         await itx.reply(message);
@@ -38,21 +76,24 @@ export const moderation = new Hashira({ name: "moderation" })
       )
       .handle(async (_, { user, reason }, itx) => {
         // TODO: Don't allow in DMs
-        if (!itx.guild || !itx.member) return;
+        if (!itx.inCachedGuild()) return;
         // TODO: Use default permissions on the slash command
         if (!itx.memberPermissions?.has(PermissionFlagsBits.BanMembers)) return;
 
         try {
           await itx.guild.members.unban(user, reason ?? undefined);
         } catch (e) {
-          // "Unknown Ban"
-          if (e instanceof DiscordAPIError && e.code === 10026) {
+          if (
+            e instanceof DiscordAPIError &&
+            e.code === RESTJSONErrorCodes.UnknownBan
+          ) {
             await itx.reply({
               content: `Użytkownik ${formatUserWithId(user)} nie ma bana.`,
               ephemeral: true,
             });
             return;
           }
+          throw e;
         }
 
         const message = reason
