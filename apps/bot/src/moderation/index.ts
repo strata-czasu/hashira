aa622a22935d08c9ccc49b392da4a19f3b4156d9
Daste <stefankar1000@gmail.com>
2024-04-10 23:34:01 +0200

    Add commands for adding and listing user warns

--- apps/bot/src/moderation/index.ts
+++ apps/bot/src/moderation/index.ts
@@ -1,4 +1,5 @@
 import { Hashira } from "@hashira/core";
 import { bans } from "./bans";
+import { warns } from "./warns";
 
-export const moderation = new Hashira({ name: "moderation" }).use(bans);
+export const moderation = new Hashira({ name: "moderation" }).use(bans).use(warns);
---
+++ apps/bot/src/moderation/warns.ts
@@ -0,0 +1,148 @@
+import { Hashira, PaginatedView } from "@hashira/core";
+import { Paginate, schema } from "@hashira/db";
+import {
+  DiscordAPIError,
+  type Guild,
+  PermissionFlagsBits,
+  RESTJSONErrorCodes,
+  TimestampStyles,
+  type User,
+  bold,
+  inlineCode,
+  italic,
+  time,
+  userMention,
+} from "discord.js";
+import { count, eq } from "drizzle-orm";
+import { base } from "../base";
+
+const formatUserWithId = (user: User) => `${bold(user.tag)} (${inlineCode(user.id)})`;
+
+const sendUserWarnMessage = async (guild: Guild, user: User, reason: string) => {
+  try {
+    await user.send(
+      `Otrzymujesz ostrzeżenie na ${bold(guild.name)}. Powód: ${italic(reason)}`,
+    );
+    return true;
+  } catch (e) {
+    if (
+      e instanceof DiscordAPIError &&
+      e.code === RESTJSONErrorCodes.CannotSendMessagesToThisUser
+    ) {
+      return false;
+    }
+    throw e;
+  }
+};
+
+export const warns = new Hashira({ name: "warns" })
+  .use(base)
+  .group("warn", (group) =>
+    group
+      .setDescription("Zarządzaj ostrzeżeniami")
+      .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+      .setDMPermission(false)
+      .addCommand("add", (command) =>
+        command
+          .setDescription("Dodaj ostrzeżenie")
+          .addUser("user", (user) => user.setDescription("Użytkownik"))
+          .addString("reason", (reason) => reason.setDescription("Powód ostrzeżenia"))
+          .handle(async ({ db }, { user, reason }, itx) => {
+            if (!itx.inCachedGuild()) return;
+
+            await db
+              .insert(schema.warn)
+              .values({
+                guildId: itx.guild.id,
+                userId: user.id,
+                moderatorId: itx.user.id,
+                reason,
+              })
+              .execute();
+
+            const sentMessage = await sendUserWarnMessage(itx.guild, user, reason);
+            // biome-ignore format: Long message
+            await itx.reply(
+              `Dodano ostrzeżenie dla ${formatUserWithId(user)}. Powód: ${italic(reason)}`,
+            );
+            if (!sentMessage) {
+              // biome-ignore format: Long message
+              await itx.followUp({
+                content: `Nie udało się wysłać wiadomości do ${formatUserWithId(user)}.`,
+                ephemeral: true,
+              });
+            }
+          }),
+      )
+      .addCommand("remove", (command) =>
+        command
+          .setDescription("Usuń ostrzeżenie")
+          .addNumber("id", (id) => id.setDescription("ID ostrzeżenia"))
+          .addString("reason", (reason) =>
+            reason.setDescription("Powód usunięcia ostrzeżenia").setRequired(false),
+          )
+          .handle(async (_, { id, reason }, itx) => {
+            // TODO: Add deleting to the database (`deleted` field? + `deleteReason`)
+            console.log(itx.user.tag, id, reason);
+            await itx.reply("Not implemented");
+          }),
+      )
+      .addCommand("edit", (command) =>
+        command
+          .setDescription("Edytuj ostrzeżenie")
+          .addNumber("id", (id) => id.setDescription("ID ostrzeżenia"))
+          .addString("reason", (reason) =>
+            reason.setDescription("Nowy powód ostrzeżenia"),
+          )
+          .handle(async (_, { id, reason }, itx) => {
+            // TODO: Editing existing warns
+            console.log(itx.user.tag, id, reason);
+            await itx.reply("Not implemented");
+          }),
+      ),
+  )
+  .group("warns", (group) =>
+    group
+      .setDescription("Wyświetl ostrzeżenia")
+      .setDMPermission(false)
+      .addCommand("user", (command) =>
+        command
+          .setDescription("Wyświetl ostrzeżenia użytkownika")
+          .addUser("user", (user) =>
+            user.setDescription("Użytkownik").setRequired(false),
+          )
+          .handle(async ({ db }, { user: selectedUser }, itx) => {
+            const user = selectedUser ?? itx.user;
+            const paginate = new Paginate({
+              orderByColumn: schema.warn.createdAt,
+              orderBy: "DESC",
+              select: db
+                .select({
+                  id: schema.warn.id,
+                  createdAt: schema.warn.createdAt,
+                  reason: schema.warn.reason,
+                  moderatorId: schema.warn.moderatorId,
+                })
+                .from(schema.warn)
+                .where(eq(schema.warn.userId, user.id))
+                .$dynamic(),
+              count: db
+                .select({ count: count() })
+                .from(schema.warn)
+                .where(eq(schema.warn.userId, user.id))
+                .$dynamic(),
+            });
+            const paginatedView = new PaginatedView(
+              paginate,
+              `Ostrzeżenia ${user.tag}`,
+              ({ id, createdAt, reason, moderatorId }, _) =>
+                `### ${userMention(moderatorId)} ${time(
+                  createdAt,
+                  TimestampStyles.ShortDateTime,
+                )} [${id}]\nPowód: ${italic(reason)}`,
+              true,
+            );
+            await paginatedView.render(itx);
+          }),
+      ),
+  );
