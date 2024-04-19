aad4e10aa1e71bbb82c77ec49c12389c8b8a0573
HKGx <19597269+HKGx@users.noreply.github.com>
2024-04-19 21:54:07 +0000

    Add a misc command that loads mutes from JSON file and inserts them into database

--- apps/bot/src/miscellaneous.ts
+++ apps/bot/src/miscellaneous.ts
@@ -1,8 +1,36 @@
 import { Hashira } from "@hashira/core";
+import { schema } from "@hashira/db";
+import { addMilliseconds } from "date-fns";
 import { AttachmentBuilder, PermissionFlagsBits } from "discord.js";
 import { base } from "./base";
 import { fetchMembers } from "./util/fetchMembers";
+import { isOwner } from "./util/isOwner";
 import { parseUserMentions } from "./util/parseUsers";
+
+interface OldMute {
+  czas_trwania: number;
+  zakonczenie: string;
+  moderator: string;
+  powod: string;
+  data_wyciszenia: string; // iso date,
+  uzytkownik: string;
+  guild_id: string;
+  id: number;
+}
+
+const toNewMute = (oldMute: OldMute): typeof schema.mute.$inferInsert => {
+  const createdAt = new Date(oldMute.data_wyciszenia);
+  const duration = addMilliseconds(createdAt, oldMute.czas_trwania);
+  console.log(createdAt, duration);
+  return {
+    guildId: oldMute.guild_id,
+    userId: oldMute.uzytkownik,
+    moderatorId: oldMute.moderator,
+    reason: oldMute.powod,
+    createdAt,
+    endsAt: duration,
+  };
+};
 
 export const miscellaneous = new Hashira({ name: "miscellaneous" })
   .use(base)
@@ -56,6 +84,46 @@ export const miscellaneous = new Hashira({ name: "miscellaneous" })
             await Promise.all(members.map((member) => member.roles.add(role.id)));
 
             await itx.editReply("Added role to users");
+          }),
+      )
+      .addCommand("load-mutes", (command) =>
+        command
+          .setDescription("Load mutes from JSON")
+          .addAttachment("mutes", (option) =>
+            option.setDescription("The mutes to load"),
+          )
+          .handle(async ({ db }, { mutes }, itx) => {
+            if (!(await isOwner(itx))) return;
+            const content = (await fetch(mutes.url).then((res) =>
+              res.json(),
+            )) as OldMute[];
+            const mutesToInsert = content.map(toNewMute);
+            const users = [
+              ...new Set(
+                mutesToInsert.flatMap((mute) => [mute.userId, mute.moderatorId]),
+              ),
+            ];
+            const userChunks = [];
+            for (let i = 0; i < users.length; i += 1000) {
+              userChunks.push(users.slice(i, i + 1000));
+            }
+            await Promise.all(
+              userChunks.map((chunk) =>
+                db
+                  .insert(schema.user)
+                  .values(chunk.map((id) => ({ id })))
+                  .onConflictDoNothing(),
+              ),
+            );
+            const chunks = [];
+            for (let i = 0; i < mutesToInsert.length; i += 1000) {
+              chunks.push(mutesToInsert.slice(i, i + 1000));
+            }
+            await Promise.all(
+              chunks.map((chunk) =>
+                db.insert(schema.mute).values(chunk).onConflictDoNothing(),
+              ),
+            );
           }),
       ),
   );
