16b193df069ea0d2b939372b8c27e90f6bd2ba31
HKGx <19597269+HKGx@users.noreply.github.com>
2024-06-28 00:55:37 +0000

    chore: Add seed script for initializing database with default data

--- apps/bot/seed.ts
+++ apps/bot/seed.ts
@@ -1,3 +1,29 @@
-import { connection } from "@hashira/db";
+import { connection, db, schema } from "@hashira/db";
+import { GUILD_IDS, STRATA_CZASU_CURRENCY, USER_IDS } from "./src/specializedConstants";
+
+const createGuild = (guildId: string) =>
+  db.insert(schema.guild).values({ id: guildId }).onConflictDoNothing();
+
+const createDefaultStrataCzasuCurrency = async (guildId: string) => {
+  await createGuild(guildId);
+  await db
+    .insert(schema.currency)
+    .values({
+      guildId,
+      name: STRATA_CZASU_CURRENCY.name,
+      symbol: STRATA_CZASU_CURRENCY.symbol,
+      createdBy: USER_IDS.Defous,
+    })
+    .onConflictDoNothing();
+};
+
+if (process.env.NODE_ENV === "production") {
+  await createDefaultStrataCzasuCurrency(GUILD_IDS.StrataCzasu);
+} else {
+  const testingServers = [GUILD_IDS.Homik, GUILD_IDS.Piwnica];
+  for (const guildId of testingServers) {
+    await createDefaultStrataCzasuCurrency(guildId);
+  }
+}
 
 await connection.end();
