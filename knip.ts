53616f55c982c775c76a5ee7113373afd7eaaeb0
HKGx <19597269+HKGx@users.noreply.github.com>
2024-07-01 19:39:10 +0000

    chore: Add Knip to devDependencies

---
+++ knip.ts
@@ -0,0 +1,18 @@
+import type { KnipConfig } from "knip";
+
+export default {
+  workspaces: {
+    "apps/bot": {
+      entry: ["src/index.ts", "reload-commands.ts"],
+      project: "**/*.ts",
+    },
+    "packages/*": {
+      entry: "index.ts",
+      project: "**/*.ts",
+    },
+    "tooling/*": {
+      entry: "index.ts",
+      project: "**/*.ts",
+    },
+  },
+} satisfies KnipConfig;
