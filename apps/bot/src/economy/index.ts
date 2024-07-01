9646bb4d6ab56478a70040199431238bc351329c
Daste <stefankar1000@gmail.com>
2024-07-01 21:31:52 +0200

    Add basic shop commands (list and add)

--- apps/bot/src/economy/index.ts
+++ apps/bot/src/economy/index.ts
@@ -1,7 +1,9 @@
 import { Hashira } from "@hashira/core";
 import { currency } from "./currency";
+import { shop } from "./shop";
 import { strataEconomy } from "./strata";
 
 export const economy = new Hashira({ name: "economy" })
   .use(currency)
-  .use(strataEconomy);
+  .use(strataEconomy)
+  .use(shop);
