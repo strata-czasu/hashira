9d67508ecf969aea6dfdaf9c04f1f3cf3f5a78ec
Daste <stefankar1000@gmail.com>
2024-07-26 10:53:28 +0200

    Make log message arguments generic per message type

--- apps/bot/src/base.ts
+++ apps/bot/src/base.ts
@@ -1,9 +1,20 @@
 import { Hashira } from "@hashira/core";
 import env from "@hashira/env";
 import { captureException } from "@sentry/bun";
+import type { Message } from "discord.js";
 import { database } from "./db";
 import { LockManager } from "./lock";
 import { Logger } from "./logger";
+
+type GuildMessage = Message<true>;
+type MessageDeleteData = {
+  message: GuildMessage;
+};
+
+type MessageEditData = {
+  oldMessage: GuildMessage;
+  newMessage: GuildMessage;
+};
 
 export const base = new Hashira({ name: "base" })
   .use(database)
@@ -14,5 +25,14 @@ export const base = new Hashira({ name: "base" })
   .const((ctx) => ({
     ...ctx,
     lock: new LockManager(),
-    log: new Logger().addMessageType("messageDelete").addMessageType("messageUpdate"),
+    log: new Logger()
+      .addMessageType("messageDelete", async (_, { message }: MessageDeleteData) => {
+        return `Message by ${message.author.tag} in ${message.channel.name} deleted: ${message.content}`;
+      })
+      .addMessageType(
+        "messageUpdate",
+        async (_, { oldMessage, newMessage }: MessageEditData) => {
+          return `Message by ${oldMessage.author.tag} in ${oldMessage.channel.name} edited: ${oldMessage.content} -> ${newMessage.content}`;
+        },
+      ),
   }));
