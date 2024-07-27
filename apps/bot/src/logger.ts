9c0323abfe95b8fe11dddd445670bbc94871a24a
Daste <stefankar1000@gmail.com>
2024-07-27 23:03:38 +0200

    Send logs to a channel in batches

--- apps/bot/src/logger.ts
+++ apps/bot/src/logger.ts
@@ -1,4 +1,7 @@
-import type { Client } from "discord.js";
+import type { Client, EmbedBuilder, TextBasedChannel } from "discord.js";
+
+// TODO)) Set correct channel ID
+const LOG_CHANNEL_ID = "464484064842219531" as const;
 
 type Prettify<T> = {
   [K in keyof T]: T[K];
@@ -8,15 +11,19 @@ type LogMessageData = { [key: string]: unknown };
 type LogMessageType = { [key: string]: LogMessageData };
 const initLogMessageTypes = {};
 
-type Handler<T> = (client: Client, data: T) => Promise<string>;
+type HandlerProps = { client: Client; timestamp: Date };
+type Handler<T> = (props: HandlerProps, data: T) => Promise<EmbedBuilder>;
+type Message<T> = { type: T; data: LogMessageData; timestamp: Date };
 
 export class Logger<
   const LogMessageTypes extends LogMessageType = typeof initLogMessageTypes,
 > {
   #messageTypes: Map<string, Handler<unknown>> = new Map();
-  #messages: Array<{ type: keyof LogMessageTypes; data: LogMessageData }> = [];
-  #interval = 5000;
+  #messages: Array<Message<keyof LogMessageTypes>> = [];
+  #interval = 5_000 as const;
   #running = false;
+  #batchSize = 5 as const;
+  #logChannel: TextBasedChannel | null = null;
 
   /**
    * Register a new log message type and define how it will be formatted
@@ -38,20 +45,55 @@ export class Logger<
    */
   async push<T extends keyof LogMessageTypes>(type: T, data: LogMessageTypes[T]) {
     if (typeof type !== "string") throw new Error("Type must be a string");
+    this.#messages.push({ type, data, timestamp: new Date() });
+  }
 
-    console.log(`push ${type} with ${data}`);
-    this.#messages.push({ type, data });
+  private consumeLogMessageBatch() {
+    const messages: Array<Message<keyof LogMessageTypes>> = [];
+    for (let i = 0; i < this.#batchSize; i++) {
+      const message = this.#messages.pop();
+      if (!message) break;
+      messages.push(message);
+    }
+    messages.reverse();
+    return messages;
+  }
+
+  private async formatLogMessage<T>(
+    client: Client,
+    { type, data, timestamp }: Message<T>,
+  ) {
+    if (typeof type !== "string") throw new Error("Type must be a string");
+    const handler = this.#messageTypes.get(type);
+    if (!handler) throw new Error(`Handler not found for ${type}`);
+    return await handler({ client, timestamp }, data);
+  }
+
+  private async getLogChannel(client: Client) {
+    if (this.#logChannel) return this.#logChannel;
+    const channel = await client.channels.fetch(LOG_CHANNEL_ID);
+    if (!channel) throw new Error(`Channel ${LOG_CHANNEL_ID} not found`);
+    if (!channel.isTextBased())
+      throw new Error(`Channel ${LOG_CHANNEL_ID} is not text based`);
+    this.#logChannel = channel;
+    return channel;
   }
 
   private async innerConsumeLoop(client: Client) {
     try {
-      console.log("Consuming log messages...");
+      const messages = this.consumeLogMessageBatch();
+      if (messages.length !== 0) {
+        // TODO)) Add limiting by message length,
+        // because our batch size could overflow a single message
+        const formatted = await Promise.all(
+          messages.map((m) => this.formatLogMessage(client, m)),
+        );
+        const channel = await this.getLogChannel(client);
+        await channel.send({ embeds: formatted });
+      }
     } catch (e) {
       console.error(e);
     }
-    // TODO)) Consume up to 5 logs
-    // TODO)) Combine them into a single message
-    // TODO)) Send to log channel
 
     setTimeout(() => this.innerConsumeLoop(client), this.#interval);
   }
