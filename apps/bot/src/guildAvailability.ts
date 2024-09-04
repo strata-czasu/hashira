4e387b2cce80cac6e84b681362d3a5e35afd8d5a
HKGx <19597269+HKGx@users.noreply.github.com.>
2024-09-04 10:31:14 +0000

    fix: Make sure bot leaves guilds that are not allowed

--- apps/bot/src/guildAvailability.ts
+++ apps/bot/src/guildAvailability.ts
@@ -1,21 +1,48 @@
 import { Hashira } from "@hashira/core";
+import { type ExtendedPrismaClient, Prisma } from "@hashira/db";
+import { partition } from "es-toolkit";
 import { base } from "./base";
+import { GUILD_IDS } from "./specializedConstants";
+
+const ALLOWED_GUILDS: string[] = Object.values(GUILD_IDS);
+
+async function processAllowedGuild(prisma: ExtendedPrismaClient, guildId: string) {
+  try {
+    await prisma.guild.create({
+      data: { id: guildId, guildSettings: { create: {} } },
+    });
+  } catch (e) {
+    // P2002: Unique constraint
+    if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002") return;
+    throw e;
+  }
+}
 
 export const guildAvailability = new Hashira({ name: "guild-availability" })
   .use(base)
   .handle("ready", async ({ prisma }, client) => {
-    const guilds = client.guilds.cache.map((guild) => ({ id: guild.id }));
+    const [allowedGuilds, unallowedGuilds] = partition(
+      [...client.guilds.cache.values()],
+      (guild) => ALLOWED_GUILDS.includes(guild.id),
+    );
+    const creationPromises = allowedGuilds.map((guild) =>
+      processAllowedGuild(prisma, guild.id),
+    );
 
-    await prisma.guild.createMany({ data: guilds, skipDuplicates: true });
-
-    const guildSettings = client.guilds.cache.map((guild) => ({ guildId: guild.id }));
-
-    await prisma.guildSettings.createMany({
-      data: guildSettings,
-      skipDuplicates: true,
+    const leavePromises = unallowedGuilds.map((guild) => {
+      console.log(`Leaving guild: ${guild.name}, owner: ${guild.ownerId}`);
+      return guild.leave();
     });
+
+    await Promise.all([...creationPromises, ...leavePromises]);
   })
   .handle("guildCreate", async ({ prisma }, guild) => {
+    if (!ALLOWED_GUILDS.includes(guild.id)) {
+      console.log(`Leaving guild: ${guild.name}, owner: ${guild.ownerId}`);
+      guild.leave();
+      return;
+    }
+
     await prisma.guild.createMany({
       data: { id: guild.id },
       skipDuplicates: true,
