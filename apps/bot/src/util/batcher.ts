8af57294e60fe9b635834ed3a8e326c8a1f170c8
Hubert <19597269+HKGx@users.noreply.github.com>
2024-09-17 23:39:54 +0200

    refactor: Make batcher generic to be reused by other modules (#84)

---
+++ apps/bot/src/util/batcher.ts
@@ -0,0 +1,54 @@
+type BatchProcessFunction<K, T> = (key: K, batch: T[]) => Promise<void>;
+
+export class Batcher<K, const T> {
+  private items: Map<K, T[]> = new Map();
+  private interval: number;
+  private batchSize: number;
+  private processing: Map<K, boolean> = new Map();
+  private processBatch: BatchProcessFunction<K, T>;
+  private enabled = false;
+
+  constructor(
+    processBatch: BatchProcessFunction<K, T>,
+    options: { interval: number; batchSize: number },
+  ) {
+    this.interval = options.interval;
+    this.batchSize = options.batchSize;
+    this.processBatch = processBatch;
+  }
+
+  push(key: K, item: T): void {
+    if (!this.items.has(key)) {
+      this.items.set(key, []);
+    }
+    const itemsForKey = this.items.get(key);
+    if (itemsForKey) {
+      itemsForKey.push(item);
+    }
+    if (!this.processing.get(key)) {
+      this.startProcessing(key);
+    }
+  }
+
+  private async startProcessing(key: K): Promise<void> {
+    if (!this.enabled) return;
+    this.processing.set(key, true);
+    while (this.items.get(key)?.length) {
+      const itemsForKey = this.items.get(key);
+      if (!itemsForKey) return;
+      // Pop the first `batchSize` items from the array
+      const batch = itemsForKey.splice(0, this.batchSize);
+      try {
+        await this.processBatch(key, batch);
+      } catch (error) {
+        console.error(`Error processing batch for key ${key}:`, error);
+      }
+      await new Promise((resolve) => setTimeout(resolve, this.interval));
+    }
+    this.processing.set(key, false);
+  }
+
+  public start(): void {
+    this.enabled = true;
+  }
+}
