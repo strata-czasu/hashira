61998aaebedf2f283a4e5fc9bceb68b948c46bc1
HKGx <19597269+HKGx@users.noreply.github.com>
2024-12-08 02:22:11 +0000

    docs: improve documentation of fetch members

--- apps/bot/src/util/fetchMembers.ts
+++ apps/bot/src/util/fetchMembers.ts
@@ -1,7 +1,20 @@
-import { Collection, type GuildMember, type UserResolvable } from "discord.js";
+import {
+  Collection,
+  type GuildMember,
+  type Snowflake,
+  type UserResolvable,
+} from "discord.js";
 import { chunk, uniq } from "es-toolkit";
 
-// TODO: holy, this type is a mess, we probably should only take in fetch method by itself
+// TODO)) The type of guild.members.fetch is very complex, but otherwise we
+// TODO)) would need to bind the fetch to members, which is not very handy.
+/**
+ * Fetches guild members in batches of 100 users at a time.
+ *
+ * Discord API limits fetching members to 100 users per request. This utility
+ * function automatically chunks the user IDs and performs parallel requests
+ * to fetch all members efficiently.
+ */
 export async function fetchMembers(
   guild: {
     members: {
@@ -10,7 +23,7 @@ export async function fetchMembers(
       >;
     };
   },
-  users: string[],
+  users: Snowflake[],
 ): Promise<Collection<string, GuildMember>> {
   const uniqueUsers = uniq(users);
   const members = chunk(uniqueUsers, 100).map((chunk) =>
