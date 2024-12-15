e2311fbbe712b7cc093cf1baf35d543647797e86
HKGx <19597269+HKGx@users.noreply.github.com>
2024-12-15 02:43:50 +0000

    chore(events): remove unused default handling functions and related types

--- packages/core/src/intents/util.ts
+++ packages/core/src/intents/util.ts
@@ -4,19 +4,6 @@ export type EventsToHandling<Events extends readonly (keyof ClientEvents)[]> = {
   [K in Events[number]]: (...args: ClientEvents[K]) => Promise<void>;
 };
 
-export type EventsToDefaultHandling<Events extends readonly (keyof ClientEvents)[]> = {
-  [K in Events[number]]: () => Promise<void>;
-};
-
-export const createEventsToDefaultHandling = <
-  Events extends readonly (keyof ClientEvents)[],
->(
-  events: Events,
-): EventsToDefaultHandling<Events> =>
-  Object.fromEntries(
-    events.map((event) => [event, async () => {}] as const),
-  ) as EventsToDefaultHandling<Events>;
-
 export type EventsToIntents<
   Events extends readonly (keyof ClientEvents)[] | readonly string[],
   Intents extends GatewayIntentBits[],
