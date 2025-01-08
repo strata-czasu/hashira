a73b6b9e4e77b13f815fae2800accb9151444612
Daste <stefankar1000@gmail.com>
2025-01-08 22:39:06 +0100

    logging: Remove timestamp from log message footers

--- apps/bot/src/logging/base/util.ts
+++ apps/bot/src/logging/base/util.ts
@@ -1,11 +1,9 @@
 import { EmbedBuilder, GuildMember, type User } from "discord.js";
 
-export const getLogMessageEmbed = (author: User | GuildMember, timestamp: Date) => {
+export const getLogMessageEmbed = (author: User | GuildMember, _timestamp: Date) => {
   const user = author instanceof GuildMember ? author.user : author;
-  return new EmbedBuilder()
-    .setAuthor({
-      name: `${user.tag} (${user.id})`,
-      iconURL: user.displayAvatarURL(),
-    })
-    .setTimestamp(timestamp);
+  return new EmbedBuilder().setAuthor({
+    name: `${user.tag} (${user.id})`,
+    iconURL: user.displayAvatarURL(),
+  });
 };
