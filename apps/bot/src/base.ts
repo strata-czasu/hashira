b7d472a8cadc1a0e4e7b8f6e30347e394e79d0f4
Daste <stefankar1000@gmail.com>
2025-02-01 22:48:59 +0100

    chore(sentry): Add more info about interactions

--- apps/bot/src/base.ts
+++ apps/bot/src/base.ts
@@ -1,6 +1,6 @@
 import { Hashira } from "@hashira/core";
 import env from "@hashira/env";
-import { captureException } from "@sentry/bun";
+import * as Sentry from "@sentry/bun";
 import { database } from "./db";
 import { LockManager } from "./lock";
 import { loggingBase } from "./logging/base";
@@ -12,9 +12,40 @@ export const base = new Hashira({ name: "base" })
   .use(messageQueueBase)
   .addExceptionHandler("default", (e, itx) => {
     if (env.SENTRY_DSN) {
-      const user = itx ? { id: itx.user.id, username: itx.user.username } : {};
-      captureException(e, { user });
+      Sentry.withScope((scope) => {
+        if (itx) {
+          scope.setUser({ id: itx.user.id, username: itx.user.username });
+          scope.setTag("interaction_type", itx.type);
+
+          if (itx.channel) {
+            scope.setExtra("channel_id", itx.channel.id);
+            scope.setExtra("channel_type", itx.channel.type);
+          }
+
+          if (itx.isCommand()) {
+            scope.setTag("command_type", itx.commandType);
+            scope.setExtra("command_deferred", itx.deferred);
+            scope.setExtra("command_ephemeral", itx.ephemeral);
+            scope.setExtra("command_replied", itx.replied);
+          }
+
+          if (itx.isChatInputCommand()) {
+            scope.setExtra("command", itx.toString());
+            scope.setTag("command_name", itx.commandName);
+          }
+
+          if (itx.isUserContextMenuCommand()) {
+            scope.setExtra("context_menu_target_user_id", itx.targetUser.id);
+          }
+          if (itx.isMessageContextMenuCommand()) {
+            scope.setExtra("context_menu_target_message_id", itx.targetMessage.id);
+          }
+        }
+
+        Sentry.captureException(e);
+      });
     }
+
     console.error(e);
   })
   .const("lock", new LockManager());
