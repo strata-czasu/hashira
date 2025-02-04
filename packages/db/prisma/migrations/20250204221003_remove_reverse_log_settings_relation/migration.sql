a3574cbc79f091e1d3b078c1b79fbee816518a50
Daste <stefankar1000@gmail.com>
2025-02-04 23:28:07 +0100

    chore(db): Remove unused logSettingsId field from guildSettings

---
+++ packages/db/prisma/migrations/20250204221003_remove_reverse_log_settings_relation/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `logSettingsId` on the `guildSettings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "guildSettings" DROP COLUMN "logSettingsId";
