d1a1716db7266cf56ddd0ae5f5e58233863e875d
HKGx <19597269+HKGx@users.noreply.github.com>
2025-02-23 01:00:02 +0000

    wip

--- apps/bot/src/userActivity/index.ts
+++ apps/bot/src/userActivity/index.ts
@@ -1,6 +1,7 @@
 import { Hashira } from "@hashira/core";
 import { userTextActivity } from "./userTextActivity";
+import { userVoiceActivity } from "./userVoiceActivity";
 
-export const userActivity = new Hashira({ name: "user-activity" }).use(
-  userTextActivity,
-);
+export const userActivity = new Hashira({ name: "user-activity" })
+  .use(userTextActivity)
+  .use(userVoiceActivity);
