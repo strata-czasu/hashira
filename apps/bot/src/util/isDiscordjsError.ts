711d1c36a93a65a3839eff7007a3769b4560693b
Daste <stefankar1000@gmail.com>
2025-02-23 23:45:05 +0100

    Revert "chore(util): Add an util for checking DiscordjsError codes"
    This reverts commit 1ce8eb0329fe3acee7b75d9c1947ec42ef289d9d.

--- apps/bot/src/util/isDiscordjsError.ts
+++
@@ -1,6 +0,0 @@
-import { DiscordjsError, type DiscordjsErrorCodes } from "discord.js";
-
-export const isDiscordjsError = (
-  e: unknown,
-  code: DiscordjsErrorCodes,
-): e is DiscordjsError => e instanceof DiscordjsError && e.code === code;
