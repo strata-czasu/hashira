5c191e81bd1ef002b13d8681666422d9161c8f4e
Daste <stefankar1000@gmail.com>
2025-02-23 23:32:58 +0100

    chore(util): Handle discordjs errors in discordTry

--- apps/bot/src/util/discordTry.ts
+++ apps/bot/src/util/discordTry.ts
@@ -1,19 +1,29 @@
-import { DiscordAPIError, type RESTJSONErrorCodes } from "discord.js";
+import {
+  DiscordAPIError,
+  DiscordjsError,
+  type DiscordjsErrorCodes,
+  type RESTJSONErrorCodes,
+} from "discord.js";
 
-interface DiscordTryOptions {
+type ErrorCodes = (DiscordjsErrorCodes | RESTJSONErrorCodes)[];
+type DiscordError = DiscordjsError | DiscordAPIError;
+interface Options {
   rethrowAfterCatch?: boolean;
 }
 
 export async function discordTry<T, U>(
   fn: () => T,
-  codes: RESTJSONErrorCodes[],
-  catchFn: (e: DiscordAPIError) => U,
-  { rethrowAfterCatch }: DiscordTryOptions = {},
+  codes: ErrorCodes,
+  catchFn: (e: DiscordError) => U,
+  { rethrowAfterCatch }: Options = {},
 ): Promise<T | U> {
   try {
     return await fn();
   } catch (e) {
-    if (e instanceof DiscordAPIError && codes.some((code) => code === e.code)) {
+    if (
+      (e instanceof DiscordjsError || e instanceof DiscordAPIError) &&
+      codes.some((code) => code === e.code)
+    ) {
       const result = await catchFn(e);
       if (!rethrowAfterCatch) return result;
     }
