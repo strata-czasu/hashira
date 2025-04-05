80299e63093304fa9967fe4001d36ad3a41134fe
Hubert <19597269+HKGx@users.noreply.github.com>
2025-04-05 13:48:57 +0200

    fix(profile): change level in profile so people don't get confused

--- apps/bot/src/profile/index.ts
+++ apps/bot/src/profile/index.ts
@@ -108,7 +108,7 @@ export const profile = new Hashira({ name: "profile" })
           .textActivity(textActivity)
           .accountCreationDate(user.createdAt)
           .exp(1234, 23001) // TODO)) Exp value
-          .level(42); // TODO)) Level value
+          .level(0); // TODO)) Level value
 
         const member = await discordTry(
           () => itx.guild.members.fetch(user.id),
