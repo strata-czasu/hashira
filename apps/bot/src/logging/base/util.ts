c287ee3d82a62da50fc462118435a2ca9b05bb6a
HKGx <19597269+HKGx@users.noreply.github.com>
2025-05-07 19:23:45 +0000

    fix(discord): fix errors after new discord.js release

--- apps/bot/src/logging/base/util.ts
+++ apps/bot/src/logging/base/util.ts
@@ -1,9 +1,14 @@
-import { EmbedBuilder, GuildMember, type User } from "discord.js";
+import { EmbedBuilder, GuildMember, type PartialUser, type User } from "discord.js";
 
-export const getLogMessageEmbed = (author: User | GuildMember, _timestamp: Date) => {
+export const getLogMessageEmbed = (
+  author: User | PartialUser | GuildMember,
+  _timestamp: Date,
+) => {
   const user = author instanceof GuildMember ? author.user : author;
+  const tag = user.tag ?? "Nieznany";
+
   return new EmbedBuilder().setAuthor({
-    name: `${user.tag} (${user.id})`,
+    name: `${tag} (${user.id})`,
     iconURL: user.displayAvatarURL(),
   });
 };
