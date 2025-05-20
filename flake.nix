823f07c18eb37df05c3eb8be55ebbc070efa3973
Daste <stefankar1000@gmail.com>
2025-05-20 21:20:20 +0000

    chore(nix): Add prisma and prisma-engines

--- flake.nix
+++ flake.nix
@@ -17,7 +17,16 @@
         devShell = mkShell {
           buildInputs = [
             pkgs-bun.bun
+            prisma
+            prisma-engines
           ];
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+          '';
         };
       }
     );
