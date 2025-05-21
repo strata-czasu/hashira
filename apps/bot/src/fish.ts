829852a41dcbbfb6224e4821bafe27836e93e4c9
Bartosz Mrugała <46654501+bartoszmrugala@users.noreply.github.com>
2025-05-21 18:39:40 +0200

    Added new fish type
    Added worthless shoe

--- apps/bot/src/fish.ts
+++ apps/bot/src/fish.ts
@@ -28,29 +28,32 @@ const checkIfCanFish = async (
 const getRandomFish = (): [string, number, number] => {
   const fishType = randomInt(1, 101);
 
+  if (fishType === 1) {
+    return ["buta", 1, 1];
+  }
   if (fishType >= 1 && fishType <= 30) {
-    return ["karasia", randomInt(30, 61), 1];
+    return ["karasia", randomInt(30, 61), 2];
   }
   if (fishType >= 31 && fishType <= 50) {
-    return ["śledzia", randomInt(50, 81), 2];
+    return ["śledzia", randomInt(50, 81), 3];
   }
   if (fishType >= 51 && fishType <= 65) {
-    return ["dorsza", randomInt(60, 91), 3];
+    return ["dorsza", randomInt(60, 91), 4];
   }
   if (fishType >= 66 && fishType <= 75) {
-    return ["pstrąga", randomInt(80, 111), 4];
+    return ["pstrąga", randomInt(80, 111), 5];
   }
   if (fishType >= 76 && fishType <= 85) {
-    return ["szczupaka :crown:", randomInt(90, 111), 5];
+    return ["szczupaka :crown:", randomInt(90, 111), 6];
   }
   if (fishType >= 86 && fishType <= 95) {
-    return ["suma", randomInt(110, 131), 6];
+    return ["suma", randomInt(110, 131), 7];
   }
   if (fishType >= 96 && fishType <= 99) {
-    return ["rekina", randomInt(150, 181), 7];
+    return ["rekina", randomInt(150, 181), 8];
   }
   if (fishType === 100) {
-    return ["bombardiro crocodilo", randomInt(900, 1101), 8];
+    return ["bombardiro crocodilo", randomInt(900, 1101), 9];
   }
 
   throw new Error("Unreachable path, all variants should've been handled above");
