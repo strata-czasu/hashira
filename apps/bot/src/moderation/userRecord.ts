c5dbbe3e170cceec44bd6090bd0cb71c7b52d96b
Daste <stefankar1000@gmail.com>
2025-05-25 14:21:25 +0200

    Expansion of /kartoteka (#143)
    * refactor(moderation): Extract /kartoteka to a separate module
    
    * feat(userRecord): Include latest mutes and warns in /kartoteka
    
    * feat(userRecord): Add relative time to account creation date
    
    * feat(userRecord): Add user verification date

---
+++ apps/bot/src/moderation/userRecord.ts
@@ -0,0 +1,118 @@
+import { Hashira } from "@hashira/core";
+import {
+  EmbedBuilder,
+  PermissionFlagsBits,
+  RESTJSONErrorCodes,
+  TimestampStyles,
+  italic,
+  time,
+} from "discord.js";
+import { base } from "../base";
+import { discordTry } from "../util/discordTry";
+import { ensureUserExists } from "../util/ensureUsersExist";
+import { formatMuteLength } from "./util";
+import { formatVerificationType } from "./verification";
+
+export const userRecord = new Hashira({ name: "user-record" })
+  .use(base)
+  .command("kartoteka", (command) =>
+    command
+      .setDescription("Sprawdź kartotekę użytkownika")
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+      .addUser("user", (user) => user.setDescription("Użytkownik"))
+      .handle(async ({ prisma }, { user }, itx) => {
+        if (!itx.inCachedGuild()) return;
+        await itx.deferReply();
+
+        await ensureUserExists(prisma, user);
+        const dbUser = await prisma.user.findFirst({ where: { id: user.id } });
+        if (!dbUser) return;
+
+        const member = await discordTry(
+          async () => itx.guild.members.fetch(user.id),
+          [RESTJSONErrorCodes.UnknownMember],
+          () => null,
+        );
+
+        const embed = new EmbedBuilder()
+          .setTitle(`Kartoteka ${user.tag}`)
+          .setThumbnail(user.displayAvatarURL({ size: 256 }))
+          .setFooter({ text: `ID: ${user.id}` })
+          .addFields({
+            name: "Data założenia konta",
+            value: `${time(user.createdAt, TimestampStyles.ShortDateTime)} (${time(user.createdAt, TimestampStyles.RelativeTime)})`,
+          });
+
+        const verificationStatusParts = [
+          formatVerificationType(dbUser.verificationLevel),
+        ];
+        const verification = await prisma.verification.findFirst({
+          where: {
+            guildId: itx.guild.id,
+            userId: user.id,
+            acceptedAt: { not: null },
+          },
+          orderBy: { acceptedAt: "desc" },
+        });
+        if (verification?.acceptedAt) {
+          verificationStatusParts.push(
+            `(przyjęto ${time(verification.acceptedAt, TimestampStyles.ShortDateTime)})`,
+          );
+        }
+        embed.addFields({
+          name: "Poziom weryfikacji",
+          value: verificationStatusParts.join(" "),
+        });
+
+        if (member) {
+          const mutes = await prisma.mute.findMany({
+            where: {
+              guildId: itx.guild.id,
+              userId: member.id,
+            },
+            orderBy: { createdAt: "desc" },
+            take: 3,
+          });
+          if (mutes.length > 0) {
+            embed.addFields({
+              name: "Ostatnie wyciszenia",
+              value: mutes
+                .map(
+                  (m) =>
+                    `${time(m.createdAt, TimestampStyles.ShortDateTime)}+${formatMuteLength(m)} ${italic(m.reason)}`,
+                )
+                .join("\n"),
+            });
+          }
+          const warns = await prisma.warn.findMany({
+            where: {
+              guildId: itx.guild.id,
+              userId: member.id,
+            },
+            orderBy: { createdAt: "desc" },
+            take: 3,
+          });
+          if (warns.length > 0) {
+            embed.addFields({
+              name: "Ostatnie ostrzeżenia",
+              value: warns
+                .map(
+                  (w) =>
+                    `${time(w.createdAt, TimestampStyles.ShortDateTime)} ${italic(w.reason)}`,
+                )
+                .join("\n"),
+            });
+          }
+        }
+
+        if (member?.joinedAt) {
+          embed.addFields({
+            name: "Data dołączenia na serwer",
+            value: `${time(member.joinedAt, TimestampStyles.ShortDateTime)} (${time(member.joinedAt, TimestampStyles.RelativeTime)})`,
+          });
+        }
+
+        await itx.editReply({ embeds: [embed] });
+      }),
+  );
--- apps/bot/src/moderation/verification.ts
+++ apps/bot/src/moderation/verification.ts
@@ -11,7 +11,6 @@ import { PaginatorOrder } from "@hashira/paginate";
 import { addSeconds } from "date-fns";
 import {
   type ChatInputCommandInteraction,
-  EmbedBuilder,
   PermissionFlagsBits,
   RESTJSONErrorCodes,
   TimestampStyles,
@@ -24,7 +23,7 @@ import {
 import { base } from "../base";
 import { STRATA_CZASU } from "../specializedConstants";
 import { discordTry } from "../util/discordTry";
-import { ensureUserExists, ensureUsersExist } from "../util/ensureUsersExist";
+import { ensureUsersExist } from "../util/ensureUsersExist";
 import { errorFollowUp } from "../util/errorFollowUp";
 import { sendDirectMessage } from "../util/sendDirectMessage";
 import {
@@ -595,54 +594,6 @@ export const verification = new Hashira({ name: "verification" })
             );
           }),
       ),
-  )
-  .command("kartoteka", (command) =>
-    command
-      .setDescription("Sprawdź kartotekę użytkownika")
-      .setDMPermission(false)
-      .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
-      .addUser("user", (user) => user.setDescription("Użytkownik"))
-      .handle(async ({ prisma }, { user }, itx) => {
-        if (!itx.inCachedGuild()) return;
-        await itx.deferReply();
-
-        await ensureUserExists(prisma, user);
-        const dbUser = await prisma.user.findFirst({ where: { id: user.id } });
-        if (!dbUser) return;
-
-        const member = await discordTry(
-          async () => itx.guild.members.fetch(user.id),
-          [RESTJSONErrorCodes.UnknownMember],
-          () => null,
-        );
-        const isInGuild = member !== null;
-
-        const embed = new EmbedBuilder()
-          .setTitle(`Kartoteka ${user.tag}`)
-          .setThumbnail(user.displayAvatarURL({ size: 256 }))
-          .setFooter({ text: `ID: ${user.id}` })
-          .addFields(
-            {
-              name: "Data założenia konta",
-              value: time(user.createdAt, TimestampStyles.ShortDateTime),
-            },
-            {
-              name: "Poziom weryfikacji",
-              value: formatVerificationType(dbUser.verificationLevel),
-            },
-            {
-              name: "Na serwerze?",
-              value: isInGuild ? "Tak" : "Nie",
-            },
-          );
-        if (isInGuild && member.joinedAt) {
-          embed.addFields({
-            name: "Data dołączenia na serwer",
-            value: time(member.joinedAt, TimestampStyles.ShortDateTime),
-          });
-        }
-        await itx.editReply({ embeds: [embed] });
-      }),
   )
   .handle("guildMemberAdd", async ({ prisma }, member) => {
     const verificationInProgress = await getActive16PlusVerification(
