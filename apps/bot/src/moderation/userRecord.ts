af013db5f953a5855815232549654c06dcbd0ec0
Daste <stefankar1000@gmail.com>
2025-05-25 13:18:43 +0200

    feat(userRecord): Include latest mutes and warns in /kartoteka

--- apps/bot/src/moderation/userRecord.ts
+++ apps/bot/src/moderation/userRecord.ts
@@ -4,11 +4,13 @@ import {
   PermissionFlagsBits,
   RESTJSONErrorCodes,
   TimestampStyles,
+  italic,
   time,
 } from "discord.js";
 import { base } from "../base";
 import { discordTry } from "../util/discordTry";
 import { ensureUserExists } from "../util/ensureUsersExist";
+import { formatMuteLength } from "./util";
 import { formatVerificationType } from "./verification";
 
 export const userRecord = new Hashira({ name: "user-record" })
@@ -53,10 +55,51 @@ export const userRecord = new Hashira({ name: "user-record" })
             },
           );
 
+        if (member) {
+          const mutes = await prisma.mute.findMany({
+            where: {
+              guildId: itx.guild.id,
+              userId: member.id,
+            },
+            orderBy: { createdAt: "desc" },
+            take: 3,
+          });
+          if (mutes.length > 0) {
+            embed.addFields({
+              name: "Ostatnie wyciszenia",
+              value: mutes
+                .map(
+                  (m) =>
+                    `${time(m.createdAt, TimestampStyles.ShortDateTime)}+${formatMuteLength(m)} ${italic(m.reason)}`,
+                )
+                .join("\n"),
+            });
+          }
+          const warns = await prisma.warn.findMany({
+            where: {
+              guildId: itx.guild.id,
+              userId: member.id,
+            },
+            orderBy: { createdAt: "desc" },
+            take: 3,
+          });
+          if (warns.length > 0) {
+            embed.addFields({
+              name: "Ostatnie ostrzeżenia",
+              value: warns
+                .map(
+                  (w) =>
+                    `${time(w.createdAt, TimestampStyles.ShortDateTime)} ${italic(w.reason)}`,
+                )
+                .join("\n"),
+            });
+          }
+        }
+
         if (member?.joinedAt) {
           embed.addFields({
             name: "Data dołączenia na serwer",
-            value: time(member.joinedAt, TimestampStyles.ShortDateTime),
+            value: `${time(member.joinedAt, TimestampStyles.ShortDateTime)} (${time(member.joinedAt, TimestampStyles.RelativeTime)})`,
           });
         }
 
