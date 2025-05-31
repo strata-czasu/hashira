518fc8775b226a0234bc214834dcea27d39dda7c
Daste <stefankar1000@gmail.com>
2025-05-31 17:00:49 +0000

    refactor(fish): Clean up the code

--- apps/bot/src/fish.ts
+++ apps/bot/src/fish.ts
@@ -34,38 +34,40 @@ const checkIfCanFish = async (
   return [canFish, nextFishing];
 };
 
-const getRandomFish = (): [string, number, number] => {
+type FishRoll = { id: number; name: string; amount: number };
+
+const getRandomFish = (): FishRoll => {
   const fishType = randomInt(1, 101);
 
   if (fishType === 1) {
-    return ["buta", 1, 1];
+    return { id: 1, name: "buta", amount: 1 };
   }
-  if (fishType >= 1 && fishType <= 30) {
-    return ["karasia", randomInt(30, 61), 2];
+  if (fishType >= 2 && fishType <= 30) {
+    return { id: 2, name: "karasia", amount: randomInt(30, 61) };
   }
   if (fishType >= 31 && fishType <= 49) {
-    return ["śledzia", randomInt(50, 81), 3];
-  }
-  if (fishType >= 51 && fishType <= 65) {
-    return ["dorsza", randomInt(60, 91), 4];
-  }
-  if (fishType >= 66 && fishType <= 75) {
-    return ["pstrąga", randomInt(80, 111), 5];
-  }
-  if (fishType >= 76 && fishType <= 85) {
-    return ["szczupaka :crown:", randomInt(90, 111), 6];
-  }
-  if (fishType >= 86 && fishType <= 95) {
-    return ["suma", randomInt(110, 131), 7];
-  }
-  if (fishType >= 96 && fishType <= 99) {
-    return ["rekina", randomInt(150, 181), 8];
-  }
-  if (fishType === 100) {
-    return ["bombardiro crocodilo", randomInt(900, 1101), 9];
+    return { id: 3, name: "śledzia", amount: randomInt(50, 81) };
   }
   if (fishType === 50) {
-    return ["wonsza żecznego", randomInt(-130, -70), 10];
+    return { id: 10, name: "wonsza żecznego", amount: randomInt(-130, -70) };
+  }
+  if (fishType >= 51 && fishType <= 65) {
+    return { id: 4, name: "dorsza", amount: randomInt(60, 91) };
+  }
+  if (fishType >= 66 && fishType <= 75) {
+    return { id: 5, name: "pstrąga", amount: randomInt(80, 111) };
+  }
+  if (fishType >= 76 && fishType <= 85) {
+    return { id: 6, name: "szczupaka :crown:", amount: randomInt(90, 111) };
+  }
+  if (fishType >= 86 && fishType <= 95) {
+    return { id: 7, name: "suma", amount: randomInt(110, 131) };
+  }
+  if (fishType >= 96 && fishType <= 99) {
+    return { id: 8, name: "rekina", amount: randomInt(150, 181) };
+  }
+  if (fishType === 100) {
+    return { id: 9, name: "bombardiro crocodilo", amount: randomInt(900, 1101) };
   }
 
   throw new Error("Unreachable path, all variants should've been handled above");
@@ -87,77 +89,76 @@ export const fish = new Hashira({ name: "fish" })
           itx.guildId,
         );
 
-        if (canFish) {
-          const [fish, amount, fishId] = getRandomFish();
-
-          await addBalance({
-            prisma,
-            currencySymbol: STRATA_CZASU_CURRENCY.symbol,
-            reason: `Łowienie ${fishId}`,
-            guildId: itx.guildId,
-            toUserId: itx.user.id,
-            amount: amount,
-          });
-
-          await prisma.lastFishing.create({
-            data: { userId: itx.user.id, guildId: itx.guildId },
-          });
-
-          const balance = formatBalance(amount, STRATA_CZASU_CURRENCY.symbol);
-
-          const reminderButton = new ButtonBuilder()
-            .setCustomId("fish_reminder")
-            .setLabel("Przypomnij mi za godzinę")
-            .setStyle(ButtonStyle.Secondary);
-
-          const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-            reminderButton,
-          );
-
-          const response = await itx.reply({
-            content: `Wyławiasz ${fish} wartego ${balance}`,
-            components: [row],
-            withResponse: true,
-          });
-
-          if (!response.resource?.message) {
-            throw new Error("Failed to receive response from interaction reply");
-          }
-
-          const clickInfo = await waitForButtonClick(
-            response.resource.message,
-            "fish_reminder",
-            { minutes: 1 },
-            (interaction) => interaction.user.id === itx.user.id,
-          );
-
-          await clickInfo.editButton((builder) => {
-            builder.setDisabled(true);
-
-            if (clickInfo.interaction) {
-              builder.setLabel("Widzimy się za godzinę");
-              builder.setStyle(ButtonStyle.Success);
-            }
-
-            return builder;
-          });
-
-          if (clickInfo.interaction) {
-            await messageQueue.push(
-              "reminder",
-              {
-                userId: itx.user.id,
-                guildId: itx.guildId,
-                text: `Możesz znowu łowić ryby! Udaj się nad wodę i spróbuj szczęścia! (${channelLink(itx.channelId, itx.guildId)})`,
-              },
-              hoursToSeconds(1),
-            );
-          }
-        } else {
+        if (!canFish) {
           await itx.reply({
             content: `Dalej masz PZW na karku. Następną rybę możesz wyłowić ${time(nextFishing, TimestampStyles.RelativeTime)}`,
             flags: "Ephemeral",
           });
+          return;
+        }
+
+        const { id, name, amount } = getRandomFish();
+
+        await addBalance({
+          prisma,
+          currencySymbol: STRATA_CZASU_CURRENCY.symbol,
+          reason: `Łowienie ${id}`,
+          guildId: itx.guildId,
+          toUserId: itx.user.id,
+          amount,
+        });
+
+        await prisma.lastFishing.create({
+          data: { userId: itx.user.id, guildId: itx.guildId },
+        });
+
+        const balance = formatBalance(amount, STRATA_CZASU_CURRENCY.symbol);
+
+        const reminderButton = new ButtonBuilder()
+          .setCustomId("fish_reminder")
+          .setLabel("Przypomnij mi za godzinę")
+          .setStyle(ButtonStyle.Secondary);
+
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(reminderButton);
+
+        const response = await itx.reply({
+          content: `Wyławiasz ${name} wartego ${balance}`,
+          components: [row],
+          withResponse: true,
+        });
+
+        if (!response.resource?.message) {
+          throw new Error("Failed to receive response from interaction reply");
+        }
+
+        const clickInfo = await waitForButtonClick(
+          response.resource.message,
+          "fish_reminder",
+          { minutes: 1 },
+          (interaction) => interaction.user.id === itx.user.id,
+        );
+
+        await clickInfo.editButton((builder) => {
+          builder.setDisabled(true);
+
+          if (clickInfo.interaction) {
+            builder.setLabel("Widzimy się za godzinę");
+            builder.setStyle(ButtonStyle.Success);
+          }
+
+          return builder;
+        });
+
+        if (clickInfo.interaction) {
+          await messageQueue.push(
+            "reminder",
+            {
+              userId: itx.user.id,
+              guildId: itx.guildId,
+              text: `Możesz znowu łowić ryby! Udaj się nad wodę i spróbuj szczęścia! (${channelLink(itx.channelId, itx.guildId)})`,
+            },
+            hoursToSeconds(1),
+          );
         }
       }),
   );
