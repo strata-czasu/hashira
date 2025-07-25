a6bbfecd8edcaac99a679f2720c60acd27a81f15
Daste <stefankar1000@gmail.com>
2025-07-25 13:39:07 +0200

    WIP API

--- apps/wordle/src/index.tsx
+++ apps/wordle/src/index.tsx
@@ -1,9 +1,10 @@
 import Env from "@hashira/env";
-import { serve } from "bun";
+import { randomUUIDv7, serve } from "bun";
 import {
   OAuth2Routes,
   type RESTPostOAuth2AccessTokenResult,
 } from "discord-api-types/v10";
+import { randomInt } from "es-toolkit";
 import * as v from "valibot";
 import index from "./frontend/index.html";
 
@@ -17,6 +18,29 @@ console.log("Using OAuth client ID:", client_id);
 const ApiTokenRequestSchema = v.object({
   code: v.string(),
 });
+
+type Game = {
+  id: string;
+  userId: string;
+  solution: string;
+};
+const games: Game[] = [];
+const GetGameRequestSchema = v.object({
+  userId: v.string(),
+});
+const GameGuessRequestSchema = v.object({
+  guess: v.string(),
+});
+
+// TODO)) Get the wordlist from DB
+const WORDLE_WORDS = ["fishy", "crane", "abcde", "ports", "fizzy"];
+
+function getRandomWord(): string {
+  // biome-ignore lint/style/noNonNullAssertion: Prototype
+  return WORDLE_WORDS[randomInt(WORDLE_WORDS.length)]!;
+}
+
+class NotFoundError extends Error {}
 
 const server = serve({
   routes: {
@@ -53,6 +77,53 @@ const server = serve({
         return Response.json({ access_token });
       },
     },
+
+    "/api/game": {
+      async POST(req) {
+        const { userId } = v.parse(GetGameRequestSchema, await req.json());
+
+        // TODO)) Use a DB
+        let game = games.find((g) => g.userId === userId);
+        if (!game) {
+          game = { id: randomUUIDv7(), userId, solution: getRandomWord() };
+          games.push(game);
+          console.log("Created game", game);
+        }
+        console.log("Found game", game);
+
+        return Response.json({
+          id: game.id,
+          userId: game.userId,
+        });
+      },
+    },
+    "/api/game/:id/guess": {
+      async POST(req) {
+        const game = games.find((g) => g.id === req.params.id);
+        if (!game) throw new NotFoundError();
+
+        const { guess } = v.parse(GameGuessRequestSchema, await req.json());
+        // TODO)) We need to know on which position the letter is correct/present/etc
+        // NOTE)) Absent letters don't require a position
+        const correct: string[] = [];
+        const present: string[] = [];
+        const absent: string[] = [];
+        let idx = 0;
+        for (const letter of guess.split("")) {
+          if (letter === game.solution[idx]) correct.push(letter);
+          else if (game.solution.includes(letter)) present.push(letter);
+          else absent.push(letter);
+          idx++;
+        }
+
+        return Response.json({
+          id: game.id,
+          correct,
+          present,
+          absent,
+        });
+      },
+    },
   },
 
   error(error) {
@@ -61,6 +132,9 @@ const server = serve({
         { message: error.message, issues: error.issues },
         { status: 400 },
       );
+    }
+    if (error instanceof NotFoundError) {
+      return Response.json({ message: "Not found" }, { status: 404 });
     }
     throw error;
   },
