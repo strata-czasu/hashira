aa9617d7e845006411f9416b9b0caee4b47e732f
Daste <stefankar1000@gmail.com>
2025-07-28 21:09:07 +0200

    chore(wordle): Split game creation/retrieval into separate endpoints

--- apps/wordle/src/error.ts
+++ apps/wordle/src/error.ts
@@ -13,6 +13,36 @@ export class NotFoundError extends ApiError {
   }
 }
 
+export class UnauthorizedError extends ApiError {
+  status = 401;
+
+  json() {
+    return { message: "Unauthorized" };
+  }
+}
+
+export class GameNotActiveError extends ApiError {
+  status = 404;
+
+  json() {
+    return { message: "Game is not active" };
+  }
+}
+
+export class GameAlreadyActiveError extends ApiError {
+  status = 400;
+  #gameId: string;
+
+  constructor(gameId: string) {
+    super();
+    this.#gameId = gameId;
+  }
+
+  json() {
+    return { message: "A game is already active", gameId: this.#gameId };
+  }
+}
+
 export class GameAlreadyFinishedError extends ApiError {
   status = 400;
   #gameId: string;
