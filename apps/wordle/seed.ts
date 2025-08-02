87fc9b99a8aea4c0109a04496ed71b16dbb598b9
Daste <stefankar1000@gmail.com>
2025-08-02 21:04:44 +0000

    feat(wordle): Store available words in the DB

---
+++ apps/wordle/seed.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@/db";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const TESTING_GUILDS = [
+  { guildId: "342022299957854220", ownerId: "195935967440404480" },
+];
+
+async function loadWordList(): Promise<string[]> {
+  const wordsFile = Bun.file("words.json");
+  const parsedWords = await wordsFile.json();
+  if (
+    !Array.isArray(parsedWords) ||
+    !parsedWords.every((word) => typeof word === "string")
+  ) {
+    throw new Error("Invalid words.json format. Expected an array of strings.");
+  }
+  return parsedWords;
+}
+
+async function createAvailableWords(guildId: string, ownerId: string) {
+  const allWords = new Set(await loadWordList());
+  const existingWords = await prisma.availableWord.findMany({
+    where: { guildId },
+    select: { word: true },
+  });
+  for (const { word } of existingWords) allWords.delete(word);
+
+  const wordsToCreate = Array.from(allWords);
+  const createdWords = await prisma.availableWord.createManyAndReturn({
+    data: wordsToCreate.map((word) => ({ guildId, createdBy: ownerId, word })),
+  });
+  console.log(
+    `Created ${createdWords.length} available words for guild ${guildId}. ${existingWords.length} already existed.`,
+  );
+}
+
+if (!isProduction) {
+  for (const { guildId, ownerId } of TESTING_GUILDS) {
+    await createAvailableWords(guildId, ownerId);
+  }
+}
+
+await prisma.$disconnect();
