c4bd5bd02804f87afd751f5586d0135e786b253d
Daste <stefankar1000@gmail.com>
2025-08-03 15:49:43 +0000

    chore(wordle): Add `TZ` to env

--- apps/wordle/env.ts
+++ apps/wordle/env.ts
@@ -6,6 +6,7 @@ const Env = v.object({
   WORDLE_OAUTH_CLIENT_ID: ID,
   WORDLE_OAUTH_CLIENT_SECRET: v.string(),
   WORDLE_DATABASE_URL: v.pipe(v.string(), v.url()),
+  TZ: v.optional(v.string()),
 });
 
 export default v.parse(Env, process.env);
