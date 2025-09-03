74b49f75d980b5cb62f7c38e305e3be74d37f7af
bnm <bartekbata1302@gmail.com>
2025-09-03 18:42:17 +0000

    replaced global var with new const cooldown class

--- apps/bot/src/pearto.ts
+++ apps/bot/src/pearto.ts
@@ -1,6 +1,6 @@
 import { TZDate } from "@date-fns/tz";
 import { Hashira } from "@hashira/core";
-import { startOfDay } from "date-fns";
+import { type Duration, addMinutes, startOfDay } from "date-fns";
 import { base } from "./base";
 import { STRATA_CZASU } from "./specializedConstants";
 
@@ -8,17 +8,35 @@ const channelId = "683025889658929231";
 const guildId = STRATA_CZASU.GUILD_ID;
 const resEmoji = "<a:peartotanczy:1410749447608205322>";
 
-let sendCooldown = 0;
+class Cooldown {
+  private lastReset: Date;
+  private readonly duration: Duration;
+
+  constructor(duration: Duration) {
+    this.duration = duration;
+    this.lastReset = new Date(0);
+  }
+
+  ended(now: Date = new Date()): boolean {
+    const endTime = addMinutes(this.lastReset, this.duration.minutes ?? 0);
+    if (now >= endTime) {
+      this.lastReset = now;
+      return true;
+    }
+    return false;
+  }
+}
 
 export const pearto = new Hashira({ name: "pearto" })
+  .const("peartoCooldown", new Cooldown({ minutes: 1 }))
   .use(base)
-  .handle("messageCreate", async ({ prisma }, message) => {
+  .handle("messageCreate", async ({ prisma, peartoCooldown }, message) => {
     if (
       message.channelId !== channelId ||
       message.author.bot ||
       !message.inGuild() ||
       message.content.trim() !== "🍐" ||
-      sendCooldown >= Date.now() - 60_000
+      !peartoCooldown.ended(message.createdAt)
     )
       return;
 
@@ -47,6 +65,4 @@ export const pearto = new Hashira({ name: "pearto" })
     message.reply(
       `${resEmoji} [${"█".repeat(fillValue)}${"░".repeat(20 - fillValue)}] ${todaysMessages}/${threshold} ${resEmoji}`,
     );
-
-    sendCooldown = Date.now();
   });
