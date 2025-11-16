aab15ec9d1a04dd51cbdc9fd320f80be254de007
HKGx <19597269+HKGx@users.noreply.github.com>
2025-11-16 04:06:27 +0000

    chore(bun): update Bun version to 1.3.2

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              bun."1.3.0"
+              bun."1.3.2"
               prisma-engines
               postgresql_17
               openssl
