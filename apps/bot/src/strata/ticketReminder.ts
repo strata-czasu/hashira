b8241ee1aca13c9dfeb7d9a2ce8b4a9b9de6ada2
Daste <stefankar1000@gmail.com>
2025-12-15 19:33:56 +0100

    feat(ticketReminder): Add role mentions to ticket reminders

--- apps/bot/src/strata/ticketReminder.ts
+++ apps/bot/src/strata/ticketReminder.ts
@@ -1,5 +1,5 @@
 import { Hashira } from "@hashira/core";
-import { channelMention, type GuildChannel } from "discord.js";
+import { channelMention, type GuildChannel, roleMention } from "discord.js";
 import { TICKET_REMINDER_SETTINGS } from "../specializedConstants";
 import { getGuildSetting } from "../util/getGuildSetting";
 
@@ -11,12 +11,16 @@ export const ticketReminder = new Hashira({ name: "ticketReminder" }).handle(
 
     if (channel.parent?.id !== settings.CATEGORY) return;
 
-    const pingChannel = await channel.client.channels.fetch(settings.TICKET_PING);
+    const pingChannel = await channel.client.channels.fetch(settings.PING_CHANNEL);
     if (!pingChannel?.isSendable()) return;
 
+    const ping = settings.PING_ROLE ? roleMention(settings.PING_ROLE) : "@here";
+
     await pingChannel.send({
-      content: `Pojawił się nowy ticket: ${channelMention(channel.id)} (${channel.name})\n\n@here`,
-      allowedMentions: { parse: ["everyone"] },
+      content: `Pojawił się nowy ticket: ${channelMention(channel.id)} (${channel.name})\n\n${ping}`,
+      allowedMentions: settings.PING_ROLE
+        ? { roles: [settings.PING_ROLE] }
+        : { parse: ["everyone"] },
     });
   },
 );
